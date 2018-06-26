43de07559f07fc0c2fa68c8b17ee438c15e177ea
nhathuy7996 <nhat.huy.7996@gmail.com>
2018-06-27 06:17:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //require our websocket library 
 var WebSocketServer = require('ws').Server; 
 
-//creating a websocket server at port 9090 
-var PORT = process.env.PORT || 9090;
+//creating a websocket server at port 80
+var PORT = process.env.PORT || 80;
 var wss = new WebSocketServer({port: PORT}); 
 
 //all connected to the server users 
