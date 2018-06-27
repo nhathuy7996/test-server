fb0c98d5dcc1a24ffb9325020c63f26302360caa
nhathuy7996 <nhat.huy.7996@gmail.com>
2018-06-27 15:57:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-const fs = require('fs');
+'use strict';
+
 const https = require('https');
+const fs = require('fs');
 const WebSocket = require('ws');
 
 const server = new https.createServer({
   cert: fs.readFileSync('cert.pem'),
   key: fs.readFileSync('key.pem')
 });
-const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+
+wss.on('connection', function connection (ws) {
+  ws.on('message', function message (msg) {
+    console.log(msg);
   });
-
-  ws.send('something');
 });
 
-var PORT = process.env.PORT || 8080;
+server.listen(function listening () {
+  //
+  // If the `rejectUnauthorized` option is not `false`, the server certificate
+  // is verified against a list of well-known CAs. An 'error' event is emitted
+  // if verification fails.
+  //
+  // The certificate used in this example is self-signed so `rejectUnauthorized`
+  // is set to `false`.
+  //
+  const ws = new WebSocket(`wss://localhost:${server.address().port}`, {
+    rejectUnauthorized: false
+  });
 
-server.listen(PORT);
+  ws.on('open', function open () {
+    ws.send('All glory to WebSockets!');
+  });
+});
