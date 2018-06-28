4eb4eb517b6f8b41be7d2ddf8e5f09b6ec479927
DESKTOP-U6G6645\nhath <nhat.huy.7996@gmail.com>
2018-06-28 10:57:53 +0700

    news

--- server.js
+++ server.js
@@ -1,36 +1,10 @@
-'use strict';
+const express = require('express');
 
-const https = require('https');
-const fs = require('fs');
-const WebSocket = require('ws');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
 
-const server = new https.createServer({
-  cert: fs.readFileSync('cert.pem'),
-  key: fs.readFileSync('key.pem')
-});
+app.get('/', (req, res) => res.render('home'));
 
-const wss = new WebSocket.Server({ server });
-wss.on('connection', function connection (ws) {
-  ws.on('message', function message (msg) {
-    console.log(msg);
-  });
-});
-
-server.listen(function listening () {
-  //
-  // If the `rejectUnauthorized` option is not `false`, the server certificate
-  // is verified against a list of well-known CAs. An 'error' event is emitted
-  // if verification fails.
-  //
-  // The certificate used in this example is self-signed so `rejectUnauthorized`
-  // is set to `false`.
-  //
-  const ws = new WebSocket(`wss://localhost:${server.address().port}`, {
-    rejectUnauthorized: false
-  });
-
-  ws.on('open', function open () {
-    ws.send('All glory to WebSockets!');
-    ws.send(server.address().port);
-  });
-});
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
